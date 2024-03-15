32a01460bf757d7978fb19fa180c68e92ac6814e
JerfesonGregorio <pro.jerfeson@gmail.com>
2024-03-15 07:55:15 -0300

    mais alteracoes

--- src/test/java/modelos/AlunoTest.java
+++ src/test/java/modelos/AlunoTest.java
@@ -1,10 +1,36 @@
 package modelos;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
 
 public class AlunoTest {
 
-    //@BeforeEach
-    //Aluno aluno1 = new Aluno();
+    Turma turma1;
+    Professor professor1;
+    Aluno aluno1;
+    Disciplina disciplina1;
+
+    @BeforeEach
+    void instanciacaoObjetos() {
+        turma1 = new Turma("1A", 1);
+        professor1 = new Professor("Itamar", 222222, "Matemática", new BigDecimal("2300.60"));
+        aluno1 = new Aluno("João", 12345, new Date(), turma1);
+        disciplina1 = new Disciplina("Matemática", 60);
+    }
+
+    @Test
+    @DisplayName("Teste de atributos")
+    void verificarAtributos() {
+        Assertions.assertEquals("João", aluno1.getNome());
+        Assertions.assertEquals(12345, aluno1.getMatricula());
+        System.out.println("Deu certo!");
+    }
 
 }
