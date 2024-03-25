68a97b175291e0338dbe59f83bc3841257f09aa8
JerfesonGregorio <pro.jerfeson@gmail.com>
2024-03-25 10:56:00 -0300

    corrigindo problemas

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,26 @@
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MainTest {
 
+    @Test
+    @Order(1)
+    public void firstTest() {
+        System.out.println("Primeiro teste");
+    }
 
+    @Test
+    @Order(3)
+    public void thirdTest() {
+        System.out.println("Terceiro teste");
+    }
 
+    @Test
+    @Order(2)
+    public void secondTest() {
+        System.out.println("Segundo teste");
+    }
 }
